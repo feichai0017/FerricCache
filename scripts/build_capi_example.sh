31fba0416326c6f3d3d396a45e0e7d71a8b1f9a8
feichai0017 <songguocheng348@gmail.com>
2025-11-24 15:13:26 +1100

    Fix capi example link order

--- scripts/build_capi_example.sh
+++ scripts/build_capi_example.sh
@@ -11,7 +11,8 @@ echo "[1/2] cargo build --release --features \"capi\""
 cargo build --release --features "capi"
 
 echo "[2/2] gcc example"
-gcc -Iinclude -Ltarget/release -lferric_cache examples/capi_example.c -o examples/capi_example
+gcc -Iinclude examples/capi_example.c target/release/libferric_cache.so -o examples/capi_example \
+  -Ltarget/release -Wl,-rpath,'$ORIGIN/../target/release'
 
 echo "Built examples/capi_example (set LD_LIBRARY_PATH=target/release when running)."
 
